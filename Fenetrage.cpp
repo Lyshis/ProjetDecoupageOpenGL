67eea1ecdded71f8b9a09d8177ff6265f152da13
MomoPython <morgan.gy55@gmail.com>
2019-02-05 10:53:50 +0100

    Avec le fenetrage qui amrche !

--- Fenetrage.cpp
+++ Fenetrage.cpp
@@ -12,34 +12,45 @@ bool coupe(std::array<int, 2> S, std::array<int, 2> Pj, std::array<int, 2> Fi, s
 }
 
 
-void inverse(std::vector<std::array<int, 2>> _matrix) {
-	int det = _matrix[0][0] * _matrix[1][1] - _matrix[1][0] * _matrix[0][1];
-	int temp = _matrix[0][0];
-	_matrix[0][0] = _matrix[1][1] / (det);
-	_matrix[1][0] = -_matrix[1][0] / (det);
-	_matrix[0][1] = -_matrix[0][1] / (det);
-	_matrix[1][1] = temp / (det);
+std::vector<std::array<float, 2>> inverse(std::vector<std::array<float, 2>> _matrix) {
+	float det = _matrix[0][0] * _matrix[1][1] - _matrix[1][0] * _matrix[0][1];
+	std::vector<std::array<float, 2>> Inv;
+	std::array<float, 2> vec1;
+	std::array<float, 2> vec2;
+	vec1[0] = _matrix[1][1] / (det);
+	vec2[0] = -_matrix[1][0] / (det);
+	vec1[1] = -_matrix[0][1] / (det);
+	vec2[1] = _matrix[0][0] / (det);
+	Inv.push_back(vec1);
+	Inv.push_back(vec2);
+	return Inv;
 }
 
-std::array<int, 2> matmul(std::vector<std::array<int, 2>> _matrix, std::array<int, 2> _vector) {
-	std::array<int, 2> retour;
+std::array<float, 2> matmul(std::vector<std::array<float, 2>> _matrix, std::array<float, 2> _vector) {
+	std::array<float, 2> retour;
 	retour[0] = _vector[0] * _matrix[0][0] +_vector[1] * _matrix[1][0];
 	retour[1] = _vector[0] * _matrix[0][1] + _vector[1] * _matrix[1][1];
 	return retour;
 }
 
 std::array<int, 2> intersection(std::array<int, 2> S, std::array<int, 2> Pj, std::array<int, 2> Fi, std::array<int, 2> Fip1) {
-	std::vector<std::array<int, 2>> matA;
-	std::array<int, 2> vec;
-	matA[0][0] = Pj[0] - S[0];
-	matA[1][0] = Fi[0] - Fip1[0];
-	matA[0][1] = Pj[1] - S[1];
-	matA[1][1] = Fi[1] - Fip1[1];
+	std::array<float, 2> matVec1;
+	std::array<float, 2> matVec2;
+	std::array<float, 2> vec;
+	matVec1[0] = Pj[0] - S[0];
+	matVec2[0] = Fi[0] - Fip1[0];
+	matVec1[1] = Pj[1] - S[1];
+	matVec2[1] = Fi[1] - Fip1[1];
+	std::vector<std::array<float, 2>> matA;
+	matA.push_back(matVec1);
+	matA.push_back(matVec2);
 	vec[0] = Fi[0] - S[0];
 	vec[1] = Fi[1] - S[1];
-	inverse(matA);
-	std::array<int, 2> retour = matmul(matA, vec);
-	int s = retour[1];
+	std::vector<std::array<float, 2>> Inv;
+	Inv = inverse(matA);
+	std::array<float, 2> vecX = matmul(Inv, vec);
+	float s = vecX[1];
+	std::array<int, 2> retour;
 	retour[0] = Fi[0] + (Fip1[0] - Fi[0])*s;
 	retour[1] = Fi[1] + (Fip1[1] - Fi[1])*s;
 	return retour;
@@ -69,42 +80,45 @@ bool visible(std::array<int, 2> S, std::array<int, 2> Fi, std::array<int, 2> Fip
 
 
 std::vector<std::array<int, 2>> algo_Sutherland_Hodgman(std::vector<std::array<int, 2>> polygone, std::vector<std::array<int, 2>> fenetre) {
-	int N2, i, j;
+	int N1, N2, i, j;
 	std::array<int, 2> S, F, I;
 	std::vector<std::array<int, 2>> PS;
 	std::vector<std::array<int, 2>> vectorRetour;
 	for (auto &pt : polygone) {
-		std::array<int, 2> temp;
-		temp[0] = pt[0];
-		temp[1] = pt[1];
-		vectorRetour.push_back(temp);
+		vectorRetour.push_back(pt);
 	}
-
-	for (i = 1; i < fenetre.size()-2;i++) {
+	std::vector<std::array<int, 2>> vectorFenetreExtend;
+	for (auto &pt : fenetre) {
+		vectorFenetreExtend.push_back(pt);
+	}
+	vectorFenetreExtend.push_back(fenetre[0]);
+	
+	N1 = vectorRetour.size();
+	for (i = 0; i < vectorFenetreExtend.size()-1;i++) {
 		N2 = 0;
 		PS.clear();
-		for (j = 1; j < vectorRetour.size() - 1; j++) {
-			if (j == 1) {
+		for (j = 0; j < N1; j++) {
+			if (j == 0) {
 				F[0] = vectorRetour[j][0];
 				F[1] = vectorRetour[j][1];
 			}
 			else {
-				if (coupe(S, vectorRetour[j],fenetre[i],fenetre[i+1])) {
-					I = intersection(S, vectorRetour[j], fenetre[i], fenetre[i + 1]);	//possiblement des erreurs sur le retour de fonction et le =
+				if (coupe(S, vectorRetour[j], vectorFenetreExtend[i], vectorFenetreExtend[i+1])) {
+					I = intersection(S, vectorRetour[j], vectorFenetreExtend[i], vectorFenetreExtend[i + 1]);
 					PS.push_back(I);
 					N2++;
 				}
 			}
 			S[0] = vectorRetour[j][0];
 			S[1] = vectorRetour[j][1];
-			if (visible(S, fenetre[i], fenetre[i + 1])) {
+			if (visible(S, vectorFenetreExtend[i], vectorFenetreExtend[i + 1])) {
 				PS.push_back(S);
 				N2++;
 			}
 		}
 		if (N2 > 0) {
-			if (coupe(S, F, fenetre[i], fenetre[i + 1])) {
-				I = intersection(S, F, fenetre[i], fenetre[i + 1]);	//possiblement des erreurs sur le retour de fonction et le =
+			if (coupe(S, F, vectorFenetreExtend[i], vectorFenetreExtend[i + 1])) {
+				I = intersection(S, F, vectorFenetreExtend[i], vectorFenetreExtend[i + 1]);
 				PS.push_back(I);
 				N2++;
 			}
@@ -115,8 +129,9 @@ std::vector<std::array<int, 2>> algo_Sutherland_Hodgman(std::vector<std::array<i
 				temp[1] = pt[1];
 				vectorRetour.push_back(temp);
 			}
+			N1 = N2;
 		}
 	}
 
-	return vectorRetour;
+	return vectorRetour; 
 }
--- Fenetrage.h
+++ Fenetrage.h
@@ -11,5 +11,5 @@ bool coupe(std::array<int, 2> S, std::array<int, 2> Pj, std::array<int, 2> Fi, s
 std::array<int, 2> intersection(std::array<int, 2> S, std::array<int, 2> Pj, std::array<int, 2> Fi, std::array<int, 2> Fip1);
 bool visible(std::array<int, 2> S, std::array<int, 2> Fi, std::array<int, 2> Fip1);
 std::vector<std::array<int, 2>> algo_Sutherland_Hodgman(std::vector<std::array<int, 2>> polygone, std::vector<std::array<int, 2>> fenetre);
-void inverse(std::vector<std::array<int, 2>> _matrix);
-std::array<int, 2> matmul(std::vector<std::array<int, 2>> _matrix, std::array<int, 2> _vector);
+std::vector<std::array<float, 2>> inverse(std::vector<std::array<float, 2>> _matrix);
+std::array<float, 2> matmul(std::vector<std::array<float, 2>> _matrix, std::array<float, 2> _vector);
